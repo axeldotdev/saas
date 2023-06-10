7fd05dcbf77d5d68f4e9a8cf3725f9d652e4ddcb
axeldotdev <axelcharpentier0@icloud.com>
2023-06-10 20:46:13 +0200

    Install spatie/laravel-google-fonts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,8 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        @googlefonts(['nonce' => csp_nonce()])
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
